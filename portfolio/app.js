04ff6594ba09ca56038b1ec0110cf141ad92e210
Aiden Letourneau <63489431+aidenletourneau@users.noreply.github.com>
2024-03-20 16:26:02 -0400

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+
+//view engine for html
+app.listen(3000);
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio');
+})
+
+app.get('/resume', (req, res) => {
+  res.render('resume');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.use((req, res) => {
+  res.status(404).render('404');
+})
